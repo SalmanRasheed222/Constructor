e2b00c22c506e55c726b8ff18b13d59b8ad530c1
SALMAN <SALMAN@DESKTOP-Q0BOMET>
2022-09-29 23:07:59 +0500

    Add project files.

---
+++ Constructor/Program.cs
@@ -0,0 +1,26 @@
+﻿using Constructor;
+
+
+//player p1 = new player();
+//student s1 = new student();
+//Console.Write("Enter Username:");
+//s1.name = Console.ReadLine();
+//Console.WriteLine(s1.name);
+//Console.WriteLine("enter Rollnumber");
+//s1.roll = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Enter Obtain Marks");
+//s1.obmarks = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("##################################################");
+//s1.show();
+
+
+student s1 = new student();
+s1.show();
+
+student s2 = new student("salman", 10, 870, 1100);
+s2.show();
+
+student s3 = new student ( s2, 845, 1100);
+s3.show();	
+
---
+++ Constructor/student.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+
+	public class player : student
+	{
+
+		public string pname { get { return name; } }
+
+		public int rno
+		{
+			get { return roll; }
+		}
+
+		public int omarks
+		{
+			get { return obmarks; }
+		}
+
+	}
+
+
+	public class student
+	{
+
+		public student()
+		{
+			name = "salman";
+			rollno = 9;
+			obmarks = 900;
+			tmarks = 1100;
+			
+		}
+
+		public student(string name,int rollno,int obmarks,int tmarks)
+		{
+			this.name = name;
+			this.rollno = rollno;
+			this.obmarks = obmarks;
+			this.tmarks = tmarks;
+		}
+
+		public student(student oldstudent,int obmarks,int tmarks )
+		{
+			this.name=oldstudent.name.Replace("Mr","");
+			this.roll = oldstudent.rollno;
+			this.obmarks=obmarks;
+			this.tmarks =tmarks;
+
+		}
+
+		public void show()
+		{
+
+			Console.WriteLine(name);
+			Console.WriteLine(rollno);
+			Console.WriteLine(obtainmarks);
+			Console.WriteLine(totalmarks);
+			Console.WriteLine(percentage);
+			Console.WriteLine("-----* Player Info *-----");
+			
+	
+		}
+
+		private string username;
+			private int rollno;
+			private int obtainmarks;
+			private int tmarks;
+			private decimal aggregate;
+
+
+		public string name
+		{
+			get { return "Mr " + username; }
+			set { username = value; }
+		}
+
+		public int obmarks
+		{
+			get { return obtainmarks;}
+			set { obtainmarks=value;}
+		}
+
+			public int roll
+			{
+			get
+			{ return rollno; }
+				
+			set
+				{
+					if (value < 0)
+					{
+						Console.WriteLine("Your Roll Number Can't be Less  Then 0. And by Default set to=");
+						rollno = 1;
+					}
+					else
+					{
+						rollno = value;
+					}
+				}
+			}
+
+			public int totalmarks
+			{
+				get { return tmarks = 1100; }
+				
+			}
+
+
+			public decimal percentage
+			{
+
+				get
+				{
+					return Convert.ToDecimal(obtainmarks) / Convert.ToDecimal(totalmarks) * 100;
+				}
+				
+			}
+
+
+		}
+
+	}
+
